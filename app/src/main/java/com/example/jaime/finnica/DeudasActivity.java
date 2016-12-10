fb40773ba0b7445885df92b1c9e208e31fb21e9b
isi4 <sas>
2016-12-09 18:41:54 -0600

    un poco mas de pago...

--- app/src/main/java/com/example/jaime/finnica/DeudasActivity.java
+++ app/src/main/java/com/example/jaime/finnica/DeudasActivity.java
@@ -3,17 +3,67 @@ package com.example.jaime.finnica;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TabHost;
 
+import com.example.jaime.finnica.clases.Ingresos;
+import com.example.jaime.finnica.clases.IngresosAdapter;
+import com.example.jaime.finnica.clases.Prestamo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DeudasActivity extends AppCompatActivity {
+    Spinner spn;
+    List<Prestamo> listaP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deudas);
 
+        spn=(Spinner) findViewById(R.id.cbxpagoG);
+        List<String> spinnerArray = new ArrayList<String>();
+
+
+
+
+
+
+        try {
+
+
+            if(Prestamo.isSugarEntity(Prestamo.class) && Prestamo.count(Prestamo.class) > 0){
+                listaP = Prestamo.listAll(Prestamo.class);
+                for(int i=0;i< Prestamo.count(Prestamo.class);i++ ){
+
+                    spinnerArray.add(listaP.get(i).getDescripcion().toString());
+
+                }
+
+
+
+            }else {
+
+                System.out.println("nada que ver pana");
+            }
+        }catch (Exception e){
+            e.getMessage();
+        }
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+
+        spn.setAdapter(adapter);
+
 
         Resources res = getResources();
+
 
         TabHost tabs=(TabHost)findViewById(android.R.id.tabhost);
         tabs.setup();
---
+++ app/src/main/java/com/example/jaime/finnica/clases/PagoAdapter.java
@@ -0,0 +1,64 @@
+package com.example.jaime.finnica.clases;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.jaime.finnica.R;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by isi4 on 09/12/2016.
+ */
+public class PagoAdapter extends ArrayAdapter<Pago> {
+
+
+    TextView descripcion;
+    TextView monto;
+    TextView fechaPago;
+    TextView prestamo;
+    SimpleDateFormat formato= new SimpleDateFormat("dd/MM/yyyy");
+
+
+    public PagoAdapter(Context context, List<Pago> objects) {
+        super(context, 0 , objects);
+    }
+
+
+
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if(convertView == null){
+            convertView = inflater.inflate(R.layout.list_item_pago, parent, false);
+        }
+
+        monto = (TextView) convertView.findViewById(R.id.txtMontoPagoItem);
+        descripcion = (TextView) convertView.findViewById(R.id.txtDescPagoItem);
+        fechaPago = (TextView) convertView.findViewById(R.id.txtFechaPagoPagoItem);
+        prestamo = (TextView) convertView.findViewById(R.id.txtPrestamoPagoItem);
+
+
+        Pago pago = getItem(position);
+
+        monto.setText(String.valueOf(pago.getMonto()));
+        descripcion.setText(pago.getDescripcion());
+        fechaPago.setText(formato.format(pago.getFechaPago()));
+        prestamo.setText(pago.getPrestamo().getAgenteFinanciero());
+
+        return convertView;
+    }
+
+}
